a6d6933ff4d17bfbb0f833859adb0d213cc441d7
john <laptopjohn10@gmail.com>
2021-08-27 17:09:17 -0400

    finish imageTools.print_all

--- imageTools.py
+++ imageTools.py
@@ -1,4 +1,7 @@
 
+
+import os
+
 from PIL import Image
 
 import imageDataTools
@@ -12,10 +15,11 @@ def get_image_int8rgb_pixels(filename):
 
 
 def save_int8rgb_tuples_as_half_hex(data, output_filename=None, mode=None, decoration="{output_filename} = \"{result}\"", **other_kwargs):
+    #does not save when output_filename is None.
     if output_filename is not None:
         if mode is None:
             raise ValueError("mode not specified")
-    result = imageDataTools.int8rgb_pixels_to_hex(chars_per_channel=1, **other_kwargs)
+    result = imageDataTools.int8rgb_pixels_to_hex(data, chars_per_channel=1, **other_kwargs)
     if output_filename is None:
         return result
     else:
@@ -27,6 +31,16 @@ def save_int8rgb_tuples_as_half_hex(data, output_filename=None, mode=None, decor
         return result
 
 
+def print_all(folder_name, file_delimiter="\n---\n", file_decoration="{name}: {content}"):
+    justStarted = True
+    for scanFileName in os.listdir(folder_name):
+        scanFullFileName = folder_name + "/" + scanFileName
+        content = save_int8rgb_tuples_as_half_hex(get_image_int8rgb_pixels(scanFullFileName))
+        printString = file_decoration.replace("{name}", scanFullFileName).replace("{content}", content)
+        if justStarted:
+            justStarted = False
+        else:
+            print(file_delimiter)
+        print(printString)  
 
-
-
+assert save_int8rgb_tuples_as_half_hex(get_image_int8rgb_pixels("images/1x1.png"), pixel_header="") =='390390390390390390390000000390390390390390390390390390390390390390000ff0ff0000390390390390390390390390390390390390000ff0ff0000390390390390390390390390390390390000ff0ff0ff0ff0000390390390390390000000000000000000ff0ff0ff0ff0000000000000000000000ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0000390000ff0ff0ff0ff0000ff0ff0000ff0ff0ff0ff0000390390390000ff0ff0ff0000ff0ff0000ff0ff0ff0000390390390390390000ff0ff0000ff0ff0000ff0ff0000390390390390390390000ff0ff0ff0ff0ff0ff0ff0ff0000390390390390390000ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0000390390390390000ff0ff0ff0ff0ff0ff0ff0ff0ff0ff0000390390390000ff0ff0ff0ff0ff0000000ff0ff0ff0ff0ff0000390390000ff0ff0ff0000000390390000000ff0ff0ff0000390000ff0ff0000000390390390390390390000000ff0ff0000000000000390390390390390390390390390390000000000'
