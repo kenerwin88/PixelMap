907c182e11dd7939bf2aa0f7dbf265304a94253b
john <laptopjohn10@gmail.com>
2021-08-27 15:18:17 -0400

    format

--- renderTiles.py
+++ renderTiles.py
@@ -2,6 +2,26 @@ import json
 from web3 import Web3, KeepAliveRPCProvider, IPCProvider
 from PIL import Image
 
+
+DEFAULT_IMAGE_DATA = "\
+    000000000000000000000000000000000000000000000000\
+    000777AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBAAA000\
+    000AAAFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDDD000\
+    000AAAFFFFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000\
+    000AAAFFFEEEEEEFFFFFFEEEEEEFFFFFFEEEEEEFFFDDD000\
+    000AAAFFFEEEFFFAAA999DDDEEEAAAAAAEEEEEEFFFDDD000\
+    000AAAFFFEEEFFF999777BBBBBB888888EEEEEEFFFDDD000\
+    000AAAFFFEEEEEEDDDBBB888888AAADDDEEEEEEFFFDDD000\
+    000AAAFFFEEEEEEEEEBBB888777AAAEEEEEEEEEFFFDDD000\
+    000AAAFFFEEEFFFAAA888AAAAAA888AAAEEEEEEFFFDDD000\
+    000AAAFFFEEEFFFAAA888DDDEEEAAA888EEEEEEFFFDDD000\
+    000AAAFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000\
+    000AAAFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000\
+    000BBBFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEEE000\
+    000AAADDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDBBB000\
+    000000000000000000000000000000000000000000000000"
+
+
 web3 = Web3(KeepAliveRPCProvider(host='localhost', port='8546'))
 print(web3.isConnected())
 abi = json.loads('[{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"owners","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"},{"name":"y","type":"uint256"}],"name":"buyTile","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"},{"name":"y","type":"uint256"}],"name":"getPos","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"urls","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"},{"name":"y","type":"uint256"}],"name":"getTile","outputs":[{"name":"","type":"address"},{"name":"","type":"string"},{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"images","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"prices","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"},{"name":"y","type":"uint256"},{"name":"image","type":"string"},{"name":"url","type":"string"},{"name":"price","type":"uint256"}],"name":"setPixel","outputs":[],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"}]')
@@ -35,6 +55,7 @@ def hex_to_rgb(value):
     lv = len(value)
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
+
 f = open("owners.csv", "w")  # opens file with name of "test.txt"
 f2 = open("urls.csv", "w")  # opens file with name of "test.txt"
 firstLoop = False
@@ -50,8 +71,8 @@ for x in range(81):    # for every pixel:
         if not url_data:
             url_data = 'pixelmap.io'
         if not image_data:
-            image_data = '000000000000000000000000000000000000000000000000000777AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBAAA000000AAAFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDDD000000AAAFFFFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000000AAAFFFEEEEEEFFFFFFEEEEEEFFFFFFEEEEEEFFFDDD000000AAAFFFEEEFFFAAA999DDDEEEAAAAAAEEEEEEFFFDDD000000AAAFFFEEEFFF999777BBBBBB888888EEEEEEFFFDDD000000AAAFFFEEEEEEDDDBBB888888AAADDDEEEEEEFFFDDD000000AAAFFFEEEEEEEEEBBB888777AAAEEEEEEEEEFFFDDD000000AAAFFFEEEFFFAAA888AAAAAA888AAAEEEEEEFFFDDD000000AAAFFFEEEFFFAAA888DDDEEEAAA888EEEEEEFFFDDD000000AAAFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000000AAAFFFEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEFFFDDD000000BBBFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEEE000000AAADDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDBBB000000000000000000000000000000000000000000000000000'
-        if (y == 48):
+            image_data = DEFAULT_IMAGE_DATA
+        if (y == 49-1):
             f.write(tile[0])
             f2.write(url_data)
         else:
@@ -74,4 +95,4 @@ for x in range(81):    # for every pixel:
         img.save('images/' + str(x) + "x" + str(y) + ".png")
 f.close()
 f2.close()
-print 'Now finished!'
+print('Now finished!')
